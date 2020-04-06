7f8b8053eeb2ff848b5ddc6733f63fe6534077d2
ian <ian.egielski@gmail.com>
2020-04-06 13:09:46 -0400

    initial commit

---
+++ MercPayraise.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents;
+
+namespace MercPayraise
+{
+    [HarmonyPatch(typeof(DefaultClanFinanceModel), "CalculateClanIncome")]
+    public class MercPayraisePatch
+    {
+        static int originalMultiplier;
+
+        static void Prefix(Clan clan, ref ExplainedNumber goldChange, bool applyWithdrawals = false)
+        {
+            originalMultiplier = clan.MercenaryAwardMultiplier;
+            clan.MercenaryAwardMultiplier *= 10;
+        }
+        static void Postfix(Clan clan, ref ExplainedNumber goldChange, bool applyWithdrawals = false)
+        {
+            clan.MercenaryAwardMultiplier = originalMultiplier;
+        }
+    }
+}
---
+++ SubModule.cs
@@ -0,0 +1,25 @@
+﻿using TaleWorlds.MountAndBlade;
+using HarmonyLib;
+using System;
+
+namespace MercPayraise
+{
+    class SubModule : MBSubModuleBase
+    {
+        public static readonly string ModuleName = "MercPayraise";
+
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            try
+            {
+                new Harmony("mod.bannerlord.mercpayraise").PatchAll();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show($"Error Initialising Bannerlord Tweaks:\n\n{ex.ToStringFull()}");
+            }
+        }
+
+    }
+}
