b702c66cbd6b9a68d2094bbd9a522e8ee38d2985
ian <ian.egielski@gmail.com>
2020-04-08 20:31:25 -0400

    1.0.8 updates

--- MercPayraise.cs
+++ MercPayraise.cs
@@ -2,11 +2,29 @@
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.SandBox.GameComponents;
 using TaleWorlds.Localization;
+using Helpers;
 using System;
 using System.Reflection;
 
 namespace MercPayraise
 {
+    class ResetMercenaryAward : CampaignBehaviorBase
+    {
+        public override void RegisterEvents()
+        {
+            CampaignEvents.OnSessionLaunchedEvent.AddNonSerializedListener(this, new Action<CampaignGameStarter>(this.OnSessionLaunched));
+        }
+
+        private void OnSessionLaunched(CampaignGameStarter starter)
+        {
+            if (Clan.PlayerClan.IsUnderMercenaryService)
+            {
+                Clan.PlayerClan.MercenaryAwardMultiplier = FactionHelper.GetMercenaryAwardFactorToJoinKingdom(Clan.PlayerClan, Clan.PlayerClan.Kingdom, false) * MercPayraiseConfig.MercPayMultiplierValue;
+            }
+        }
+
+        public override void SyncData(IDataStore dataStore) { }
+    }
     // Patch GetMercenaryAwardFactor to multiply the player's payment for working as a mercenary.
     [HarmonyPatch]
     public class LordConversationsCampaignBehaviorPatch
@@ -18,20 +36,7 @@ namespace MercPayraise
 
         static void Postfix(ref int __result)
         {
-            __result = __result * 10;
-        }
-    }
-
-    // Patch CalculateClanIncome to fix a bug where the contract payment would be calculated as zero.
-    [HarmonyPatch(typeof(DefaultClanFinanceModel), "CalculateClanIncome")]
-    public class MercPayraisePatch
-    {
-        static void Postfix(Clan clan, ref ExplainedNumber goldChange, bool applyWithdrawals = false)
-        {
-            if (clan == Clan.PlayerClan && clan.Influence * 0.1f < 1)
-            {
-                goldChange.Add(clan.MercenaryAwardMultiplier, new TextObject("{=qcaaJLhx}Mercenary Contract", null));
-            }
+            __result = __result * MercPayraiseConfig.MercPayMultiplierValue;
         }
     }
 }
--- SubModule.cs
+++ SubModule.cs
@@ -1,6 +1,10 @@
-﻿using TaleWorlds.MountAndBlade;
+﻿using System;
 using HarmonyLib;
-using System;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.Engine.Screens;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
 
 namespace MercPayraise
 {
@@ -10,16 +14,30 @@ namespace MercPayraise
 
         protected override void OnSubModuleLoad()
         {
-            base.OnSubModuleLoad();
+            MercPayraiseConfig.Initialize();
+            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("MercPayraiseConfig", new TextObject("Mercenary Payraise Options"), 9998, delegate
+            {
+                ScreenManager.PushScreen(new MercPayraiseGauntletScreen());
+            }, false));
+            
             try
             {
                 new Harmony("mod.bannerlord.mercpayraise").PatchAll();
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
                 // TODO: Handle this.
             }
         }
 
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            // Check that the game type is a campaign and return if it isn't.
+            if (game.GameType as Campaign == null)
+            {
+                return;
+            }
+            ((CampaignGameStarter)gameStarterObject).AddBehavior(new ResetMercenaryAward());
+        }
     }
 }
